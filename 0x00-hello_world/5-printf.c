bdfbf4a803478de41f68f606edcb1e807a7f0a53
Jared Murundu <mmurundujared@gmail.com>
2023-08-20 03:46:36 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entery point
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
